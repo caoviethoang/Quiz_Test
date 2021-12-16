62625155111f560801caddd21e3a83ab65008421
caoviethoang <hoangcv17@gmail.com>
2021-12-15 23:29:44 -0800

    admin only login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :answers
   resources :categories
   devise_for :admins
-  root to: "home#index"
+  root to: "admin#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

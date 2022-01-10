018c3843de627137b7fd86ff26270c40f56b3ce6
caoviethoang <hoangcv17@gmail.com>
2022-01-10 01:43:02 -0800

    fix commit

--- app/javascript/packs/custom-exam.js
+++ app/javascript/packs/custom-exam.js
@@ -1,26 +1,18 @@
-const start_btn = document.querySelector(".start-btn button");
-const info_box = document.querySelector(".info-box");
-const exit_btn = document.querySelector(".start .quit");
-const continue_btn = document.querySelector(".start .continue");
+$(".content-start").on("click", ".show-info", function () {
+  $(this).addClass("info-box");
+});
 
-start_btn.onclick = () => {
-  info_box.classList.add("activeInfo");
-};
+$(".content-start").on("click", ".remove-info", function () {
+  $(this).removeClass("info-box");
+});
 
-exit_btn.onclick = () => {
-  info_box.classList.remove("activeInfo");
-};
+$(".row").on("click", ".start-exam", function () {
+  $(this).removeClass("info-box");
+  $(".content-start").css("display", "none");
 
-continue_btn.onclick = () => {
-  info_box.classList.remove("activeInfo");
-  document.getElementById("content-start").style.display = "none";
+  $(".content-question")
+    .css("display", "block")
+    .addClass("content-question col-12 col-md-9");
 
-  var question = document.getElementById("content-question");
-  var side = document.getElementById("right-bar");
-
-  question.style.display = "block";
-  side.style.display = "block";
-
-  question.className += "content-question col-12 col-md-9";
-  side.className += "right-bar col-6 col-md-3";
-};
+  $(".right-bar").css("display", "block").addClass("right-bar col-6 col-md-3");
+});

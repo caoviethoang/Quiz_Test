1330155c0ed30e95928ea87b1834ef4fe4fc8ab6
Cao Việt Hoàng <76580781+caoviethoang@users.noreply.github.com>
2022-04-05 13:20:12 +0700

    Feature/exam (#12)
    * Add CRUD Question
    
    * update delete question
    
    * fix add question
    
    * add multiple answers
    
    * render form
    
    * add and remove answers
    
    * remove Answer
    
    * export Questions to Exam
    
    * create and save exam
    
    * fix show exam
    
    * fix commit
    
    * fix code
    
    * generate exam and countdown
    
    * exam process and candidate score ranking
    
    * fix code prevent double submit
    
    * Add image to question (#10)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :category
   has_many :answers, dependent: :destroy
+  has_one_attached :image
+  
+  validates :title, presence: true
   
   validates :title, presence: true
   
